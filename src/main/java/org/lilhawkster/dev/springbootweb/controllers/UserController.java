c5d7956a8cbc30923c269c8ebfd12d03cb7dbe51
Andres Mosquera <qualitycolombia.dvp09@gmail.com>
2025-03-09 21:31:17 -0500

    Second commit, adding a model to send data from the controller to the thymeleaf view

--- src/main/java/org/lilhawkster/dev/springbootweb/controllers/UserController.java
+++ src/main/java/org/lilhawkster/dev/springbootweb/controllers/UserController.java
@@ -1,13 +1,17 @@
 package org.lilhawkster.dev.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+        model.addAttribute("title", "Hola mundo Spring Boot Web");
+        model.addAttribute("name", "Andres");
+        model.addAttribute("lastname", "Mosquera");
         return "details";
     }
 }
